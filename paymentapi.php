fc3f0aba9fe2642d44a6393aa14178a14e715ce2
Ashish Vishwakarma <97900352+vishwakarmaashish801@users.noreply.github.com>
2022-06-13 18:17:07 +0530

    Add files via upload

---
+++ paymentapi.php
@@ -0,0 +1,92 @@
+<?php
+session_start();
+//connect to database
+require_once("dbcontroller.php");
+$db_handle = new DBController();
+
+//required variables
+$jsontosend=$_SESSION['payjson'];
+$orderid=$_SESSION['orderid'];
+$username=$_SESSION['username'];
+$order_details=json_encode($_SESSION['cart_item']);
+$amount=$_SESSION['totalprice'];
+
+
+//send to pay api
+$apiurl="https://api.na.bambora.com/v1/payments";
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $apiurl);
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $jsontosend);
+//replace ABcdEFGh.... with encoded code of in the format merchantid:paymentapi
+//paymentapi key is different from main api key
+//go to google and search for base64encoded and encode your code in it and paste it here instead AbcdEFghIJ.. here
+curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'Authorization: Passcode MzAwMjEyODM5OkVFYjdBMTQ5N2JkRTQ4MjVhZTQyQTY0ZTkzYWQ5OEY0',
+      'Content-Type: application/json',
+   ));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$resultfromserver = curl_exec($curl);
+$paid=json_decode($resultfromserver,true);
+//to print result
+$approved=$paid['approved'];
+$transaction_id=$paid['id'];
+
+//update orders
+if($approved==1)
+{
+     $db_handle->runQuery(" INSERT INTO orders (username,order_details,transaction_id ,order_id ,amount) VALUES ('$username','$order_details','$transaction_id','$orderid','$amount') ");
+}
+
+?>
+
+<html>
+<head>
+<title><?php if($approved==1){echo "Success";}else{echo "Payment failed!";}?>
+</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+      <script>
+var something = (function() {
+    var executed = false;
+    return function() {
+        if (!executed) {
+            window.open("https://crazygeeks.in/"); 
+            executed = true;
+            
+        }
+    };
+})();
+</script>
+</head>
+<body>
+
+<center>
+<?php 
+if($approved==1){
+     echo '<table class="table table-bordered table-striped">';
+    echo '<th class="text-center">Success! Your payment is approved.</th>';
+    echo '<tr class="info text-center"><td>';
+    echo 'Transaction id :'.$transaction_id.'</td></tr>';
+    echo '<tr class="info text-center"><td>';
+    echo 'Order id : '.$orderid.'</td></tr>';
+    echo '<tr><td><input type="button" value="View orders" class="btn btn-info" onclick="orders()" >';
+    echo '</td></tr></table>';
+
+    }
+    else{
+        echo "<h2>Payment failed! Your payment is declined.</h2>";
+        echo "<br>";
+        echo "<h3>contact support</h3>";
+        }
+?>
+</center>
+
+
+<script>
+function orders(){
+      //replace the location accordingly
+    window.location.href="orders.php";
+}
+</script>
+</body>
+</html>
---
+++ profileapi.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+
+//json file generated to create a  payment profile in bambora using token
+$jsontosend=$_SESSION['profilejson'];
+//total price of the order
+$amount=$_SESSION['totalprice'];
+//generated order id
+$orderid=$_SESSION['orderid'];
+//set up api to send
+$apiurl="https://api.na.bambora.com/v1/profiles";
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $apiurl);
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $jsontosend);
+//replace the "ABcdEFghIjKLmnOPqrStUvwwxyz" with the base64encoded(merchantid:apikey)
+curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'Authorization: Passcode MzAwMjEyODM5Ojk2RTlEQzRFNjVDNjRDREQ4RUU5NUYwRjRBRjYxMzA5',
+      'Content-Type: application/json',
+   ));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$resultfromserver = curl_exec($curl);
+$customer=json_decode($resultfromserver,true);
+$customerid=$customer['customer_code'];
+$_SESSION['customerid']=($customerid); 
+
+
+//json for payment
+$datatosend = array("amount" => $amount,
+                    "order_number" => "".$orderid."",
+                    "payment_method" => "payment_profile",
+                    "payment_profile" => array("customer_code" => $customerid,
+                                            "card_id" => "1",
+                                            "complete" => "true"
+                                       ));
+$encodedjson=json_encode($datatosend);
+$_SESSION['payjson']=$encodedjson;
+
+?>
+<html>
+<head><title>Payment Profile</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+      <script>
+var something = (function() {
+    var executed = false;
+    return function() {
+        if (!executed) {
+            window.open("https://crazygeeks.in/"); 
+            executed = true;
+            
+        }
+    };
+})();
+</script>
+</head>
+<body>
+<?php
+
+
+//display the results
+if(isset($customerid)){
+    echo '<table class="table table-bordered table-striped">';
+    echo '<th class="text-center">Congratulations! Your payment profile is ready</th>';
+    echo '<tr class="success text-center"><td>click here to pay</td></tr><tr class="text-center"><td>';
+    echo '<input type="button" class="btn btn-success" onclick="pay()" value="Pay $ ';
+    echo $_SESSION['totalprice'];
+    echo ' "name="pay" >';
+    echo '</td></tr></table>';
+}
+else{
+    echo 'Go back! Contact support';
+}
+
+?>
+
+<script>
+function pay(){
+      //replace the location with your file location
+    window.location.href = "paymentapi.php";
+}
+</script>
+</body>
+</html>
